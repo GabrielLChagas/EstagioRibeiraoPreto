1820e0177d9c6a08d185929c4299342977f83e68
GabrielLChagas <Chagasg280@outlook.com>
2024-09-15 22:51:10 -0300

    Atualização2

---
+++ EstagioRibeirao/src/Atividades/Atividade1.java
@@ -0,0 +1,35 @@
+package Atividades;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Atividade1 {
+
+    Scanner numero = new Scanner(System.in);
+
+    ArrayList fibonacci = new ArrayList<>();
+
+    int numEscolhido;
+
+    public int selecionaNumero(){
+        System.out.println("\nInsira o número escolhido: ");
+        numEscolhido = numero.nextInt();
+        return numEscolhido;
+    }
+
+    public void calcular(){
+        Integer eSoma = -1;
+        Integer e = 1;
+        do {int eA = e;
+            e += eSoma;
+            fibonacci.add(e);
+            eSoma = eA;
+        } while (fibonacci.size() < 10);
+
+        if (fibonacci.contains(numEscolhido)){
+            System.out.println("O valor " + numEscolhido + " é um valor de Fibonacci.\n");
+        } else {System.out.println("O valor " + numEscolhido + " não é um valor de Fibonacci.\n");
+        }
+    }
+
+}
